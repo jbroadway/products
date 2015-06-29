d9d517647d833c2f3a325c7c3656822e4d639084
Johnny Broadway <johnny@johnnybroadway.com>
2015-06-28 20:54:08 -0500

    Added discount integration

--- models/Product.php
+++ models/Product.php
@@ -20,6 +20,17 @@ namespace products;
 class Product extends \Model {
 	public $table = '#prefix#products';
 	public $key = 'id';
+	
+	/**
+	 * Get the product's discount price based on the specified discount
+	 * value, which is an integer.
+	 */
+	public function discount_price ($discount = 0) {
+		if ($discount > 0) {
+			return $this->price - ($this->price * ($discount / 100));
+		}
+		return $this->price;
+	}
 }
 
 ?>
